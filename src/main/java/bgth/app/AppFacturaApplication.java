22932a29583eaddee902d09c61cd6738117c384f
Byron Gonzalez <ing.byrongonzalez@gmail.com>
2023-08-24 11:03:11 -0600

    Proyecto maven creado con las dependencias Spring Boot DevTools, Spring Data JPA, Spring web, MySQL Driver

---
+++ src/main/java/bgth/app/AppFacturaApplication.java
@@ -0,0 +1,13 @@
+package bgth.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppFacturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppFacturaApplication.class, args);
+	}
+
+}
