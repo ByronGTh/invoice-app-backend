fbed9fd94dc40bca22ca1b1a8a46fc3270fe0784
Byron Gonzalez <ing.byrongonzalez@gmail.com>
2023-08-24 18:25:11 -0600

    package model y package entities creados

---
+++ src/main/java/bgth/app/model/entities/Producto.java
@@ -0,0 +1,46 @@
+package bgth.app.model.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Producto {
+	
+	@Id
+	@Column(name = "idproducts")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Long getPrice() {
+		return price;
+	}
+	public void setPrice(Long price) {
+		this.price = price;
+	}
+	private String name;
+	private String description;
+	private Long price;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
