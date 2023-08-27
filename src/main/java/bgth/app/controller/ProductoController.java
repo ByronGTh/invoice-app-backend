14b611ec00d114ffafa28b8cdba34eb1c505bf77
Byron Gonzalez <ing.byrongonzalez@gmail.com>
2023-08-26 23:23:19 -0600

    ProductoController.java: Concediendo permiso al frontend con @CrossOrigin para que este pueda acceder a extraer datos al backend

--- src/main/java/bgth/app/controller/ProductoController.java
+++ src/main/java/bgth/app/controller/ProductoController.java
@@ -3,6 +3,7 @@ package bgth.app.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import bgth.app.model.entities.Producto;
 import bgth.app.service.IProductoService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 public class ProductoController {
 	
 	@Autowired
