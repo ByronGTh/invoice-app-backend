6f4c27b7353f40d9b890d9fdb69f8c70117aadf2
Byron Gonzalez <ing.byrongonzalez@gmail.com>
2023-08-28 10:36:24 -0600

    src/main/java/bgth/app/service/IProductoService.java: Agregados los metodos buscarPorId, guardar y eliminar

--- src/main/java/bgth/app/service/IProductoService.java
+++ src/main/java/bgth/app/service/IProductoService.java
@@ -1,10 +1,14 @@
 package bgth.app.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import bgth.app.model.entities.Producto;
 
 public interface IProductoService {
 
 	List<Producto> mostrarRegistros();
+	Optional<Producto> buscarPorId(Long id);
+	Producto guardar(Producto producto);
+	void eliminar(Long id);
 }
