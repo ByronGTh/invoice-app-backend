ce7decfd7f1453f58bb3cda4f070d2f993838744
Byron Gonzalez <ing.byrongonzalez@gmail.com>
2023-08-28 10:39:02 -0600

    src/main/java/bgth/app/service/ProductoServiceImpl.java: Implementados metodos agregados al IProductoService y aplicada la logica a cada uno

--- src/main/java/bgth/app/service/ProductoServiceImpl.java
+++ src/main/java/bgth/app/service/ProductoServiceImpl.java
@@ -1,9 +1,11 @@
 package bgth.app.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import bgth.app.model.entities.Producto;
 import bgth.app.repository.ProductoRepository;
@@ -15,8 +17,27 @@ public class ProductoServiceImpl implements IProductoService{
 	private ProductoRepository repository;
 	
 	@Override
+	@Transactional(readOnly = true)
 	public List<Producto> mostrarRegistros() {
 		return (List<Producto>)repository.findAll();
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Optional<Producto> buscarPorId(Long id) {
+		return repository.findById(id);
+	}
+
+	@Override
+	@Transactional
+	public Producto guardar(Producto producto) {
+		return repository.save(producto);
+	}
+
+	@Override
+	@Transactional
+	public void eliminar(Long id) {
+		repository.deleteById(id);
 	}
 
 }
