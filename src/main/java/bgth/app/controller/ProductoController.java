933b1b59cc58ed493c139c748ff15458aacf817d
Byron Gonzalez <ing.byrongonzalez@gmail.com>
2023-08-28 11:23:32 -0600

    ProductoController.java: Metodo para buscar producto por su ID creado

--- src/main/java/bgth/app/controller/ProductoController.java
+++ src/main/java/bgth/app/controller/ProductoController.java
@@ -1,8 +1,11 @@
 package bgth.app.controller;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,4 +24,14 @@ public class ProductoController {
 	public List<Producto> listaDeProductos(){
 		return service.mostrarRegistros();
 	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> show(Long id){
+		Optional<Producto> opProducto = service.buscarPorId(id);
+		if (opProducto.isPresent()) {
+			return ResponseEntity.ok(opProducto.orElseThrow());
+		}
+		return ResponseEntity.notFound().build();
+	}
+	
 }
