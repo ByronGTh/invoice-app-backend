5811b31417d0b448af701629772ce25ddf9510ca
Byron Gonzalez <ing.byrongonzalez@gmail.com>
2023-08-28 22:56:30 -0600

    ProductoController.java: Metodo post agregado

--- src/main/java/bgth/app/controller/ProductoController.java
+++ src/main/java/bgth/app/controller/ProductoController.java
@@ -3,11 +3,13 @@ package bgth.app.controller;
 import java.util.List;
 import java.util.Optional;
 
-import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import bgth.app.model.entities.Producto;
@@ -32,6 +34,11 @@ public class ProductoController {
 			return ResponseEntity.ok(opProducto.orElseThrow());
 		}
 		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	public ResponseEntity<?> create(@RequestBody Producto producto){
+		return ResponseEntity.status(HttpStatus.CREATED).body(service.guardar(producto));
 	}
 	
 }
