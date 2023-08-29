7d4e6e7019857330bc8dce6c24fed81d4262f23f
Byron Gonzalez <ing.byrongonzalez@gmail.com>
2023-08-28 23:14:12 -0600

    ProducotController.java: Metodo put agregado

--- src/main/java/bgth/app/controller/ProductoController.java
+++ src/main/java/bgth/app/controller/ProductoController.java
@@ -8,7 +8,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,6 +41,19 @@ public class ProductoController {
 	@PostMapping
 	public ResponseEntity<?> create(@RequestBody Producto producto){
 		return ResponseEntity.status(HttpStatus.CREATED).body(service.guardar(producto));
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<?> update(@RequestBody Producto producto, @PathVariable Long id){
+		Optional<Producto> optional = service.buscarPorId(id);
+		if (optional.isPresent()) {
+			Producto productoDb = optional.orElseThrow();
+			productoDb.setName(producto.getName());
+			productoDb.setDescription(producto.getDescription());
+			productoDb.setPrice(producto.getPrice());
+			return ResponseEntity.status(HttpStatus.CREATED).body(service.guardar(productoDb));
+		}
+		return ResponseEntity.notFound().build();
 	}
 	
 }
