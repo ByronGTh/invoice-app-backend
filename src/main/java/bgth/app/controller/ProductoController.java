469b53049b3b5912ee99d655dde62661065e0ba9
Byron Gonzalez <ing.byrongonzalez@gmail.com>
2023-08-29 00:24:46 -0600

    ProductoController.java: Metodo Delete agregado

--- src/main/java/bgth/app/controller/ProductoController.java
+++ src/main/java/bgth/app/controller/ProductoController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -52,6 +53,16 @@ public class ProductoController {
 			productoDb.setDescription(producto.getDescription());
 			productoDb.setPrice(producto.getPrice());
 			return ResponseEntity.status(HttpStatus.CREATED).body(service.guardar(productoDb));
+		}
+		return ResponseEntity.notFound().build();
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> remove(@PathVariable Long id){
+		Optional<Producto> o = service.buscarPorId(id);
+		if (o.isPresent()) {
+			service.eliminar(id);
+			return ResponseEntity.noContent().build();
 		}
 		return ResponseEntity.notFound().build();
 	}
