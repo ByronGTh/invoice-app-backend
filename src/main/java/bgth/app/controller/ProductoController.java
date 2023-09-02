16d611dab4fbb155418439743568e808bc6e64ea
Byron Gonzalez <ing.byrongonzalez@gmail.com>
2023-09-01 19:21:40 -0600

    ProductoController.java: @PathVariable agregado en parametros del metodo update

--- src/main/java/bgth/app/controller/ProductoController.java
+++ src/main/java/bgth/app/controller/ProductoController.java
@@ -13,25 +13,27 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import bgth.app.model.entities.Producto;
 import bgth.app.service.IProductoService;
 
 @RestController
+@RequestMapping("/productos")
 @CrossOrigin(origins = "http://localhost:5173")
 public class ProductoController {
 	
 	@Autowired
 	private IProductoService service;
 	
-	@GetMapping("/productos")
+	@GetMapping
 	public List<Producto> listaDeProductos(){
 		return service.mostrarRegistros();
 	}
-	
+	//Todo: Quitar el @PathVariable para que se produzca el error capturarlo para identificarlo en futras ocaciones....
 	@GetMapping("/{id}")
-	public ResponseEntity<?> show(Long id){
+	public ResponseEntity<?> show(@PathVariable Long id){
 		Optional<Producto> opProducto = service.buscarPorId(id);
 		if (opProducto.isPresent()) {
 			return ResponseEntity.ok(opProducto.orElseThrow());
@@ -46,7 +48,7 @@ public class ProductoController {
 	
 	@PutMapping("/{id}")
 	public ResponseEntity<?> update(@RequestBody Producto producto, @PathVariable Long id){
-		Optional<Producto> optional = service.buscarPorId(id);
+		Optional<Producto> optional = service.actualizar(producto, id);
 		if (optional.isPresent()) {
 			return ResponseEntity.status(HttpStatus.CREATED).body(optional.orElseThrow());
 		}
